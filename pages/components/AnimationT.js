f8aa82787c9917cfd8c50b4607ddaacfeda7f865
haypritam <haypritam@gmail.com>
2024-08-15 17:42:04 +0530

    s

--- pages/components/AnimationT.js
+++ pages/components/AnimationT.js
@@ -1,24 +1,15 @@
-import React from 'react'
+import React from 'react';
 
- const AnimationT = ({text, className=""}) => {
+const AnimationT = ({ text = "", className = "" }) => {
     return (
-        <div className='w-full mx-auto py-2 flex items-center justify-center text-center
-        overflow-hidden'>
-            <h1 className={`inline-block w-full text-dark font-bold capitalize text-4xl ${className}`}
-            >
-                {
-                    text.split('').map((word,index) =>
-                    <span key={word+'-'+index}>{word}&nbsp;</span>
-                    )
-                }
-
+        <div className='w-full mx-auto py-2 flex items-center justify-center text-center overflow-hidden'>
+            <h1 className={`inline-block w-full text-dark font-bold capitalize text-4xl ${className}`}>
+                {text.split('').map((word, index) => (
+                    <span key={word + '-' + index}>{word}&nbsp;</span>
+                ))}
             </h1>
-
-
-
         </div>
-    )
-    }
+    );
+}
 
 export default AnimationT;
-    
