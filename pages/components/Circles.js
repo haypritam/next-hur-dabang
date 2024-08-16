5ad1e0b657b8266d4d90cf5162916e35d0113cfb
haypritam <haypritam@gmail.com>
2024-08-16 08:31:31 +0530

    circles

--- pages/components/Circles.js
+++ pages/components/Circles.js
@@ -1,26 +1,27 @@
-import React from 'react'
-import { motion, useScroll } from 'framer-motion'
+import React from 'react';
+import { motion, useScroll } from 'framer-motion';
 
-const Circles = ({reference}) => {
-
-    const {scrollyProgress} =useScroll(
-        {
-            target: reference,
-            offset: ['center end', 'center center']
-        }
-    )
+const Circles = ({ reference }) => {
+  const { scrollYProgress } = useScroll({
+    target: reference,
+    offset: ['center end', 'center center'],
+  });
 
   return (
-   <figure className='absolute left-0 stroke-dark'>
-    <svg width='75' height='75' viewBox='0 0 100 100'>
-        <motion.circle cx='75' cy='50' r='20' className='stroke-primary stroke-1 fill-none' />
-         style={{
-            pathLength : scrollyProgress
-         }}
-
-    </svg>
-   </figure>
-  )
-}
+    <figure className='absolute left-0 stroke-dark'>
+      <svg width='75' height='75' viewBox='0 0 100 100'>
+        <motion.circle
+          cx='75'
+          cy='50'
+          r='20'
+          className='stroke-primary stroke-1 fill-none'
+          style={{
+            pathLength: scrollYProgress,
+          }}
+        />
+      </svg>
+    </figure>
+  );
+};
 
 export default Circles;
